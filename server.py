663a9b1cc21ffcce765b873709e7821558f7f9cf
Paul Kiernan <paul@chartbeat.com>
2017-04-29 18:16:07 -0400

    SGet a turtle pointmodel working and animating in threejs

---
+++ server.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" BIXI909 Webserver
+
+Super basic webserver for serving static assets for (hopefully) a bixi
+simulator.
+"""
+
+# Standard Library Imports
+import argparse
+import logging
+from logging.handlers import RotatingFileHandler
+
+# Third Party Library Imports
+import gevent
+assert gevent  # Silence linting
+from gevent.wsgi import WSGIServer
+
+# Flask Imports
+from flask import Flask
+from flask import render_template
+
+# Global Variables
+APP = Flask(__name__)
+
+
+@APP.route('/')
+def index():
+    return render_template('index.html')
+
+
+@APP.route('/sim')
+def simulator():
+    return render_template('bixi.html')
+
+
+if __name__ == "__main__":
+
+    argument_parser = argparse.ArgumentParser(
+        description=(
+            "BIXI909 Webserver. Basic program for serving static assets for "
+            "simulating lighting arrangements on our beloved Bixi."
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    argument_parser.add_argument(
+        "--debug",
+        default=False,
+        action="store_true",
+        help="Enable debug logging and handlers (including refresh).",
+    )
+    argument_parser.add_argument(
+        "--gevent",
+        default=False,
+        action="store_true",
+        help="Use greenlet backed threading model instead of Flask's.",
+    )
+    args = argument_parser.parse_args()
+
+    APP.debug = args.debug
+
+    if args.gevent:
+        server = WSGIServer(("", 5000), APP)
+        server.serve_forever()
+    else:
+        APP.run()
